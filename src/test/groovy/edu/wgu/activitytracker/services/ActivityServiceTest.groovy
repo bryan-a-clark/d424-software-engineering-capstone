96651652904552054222472d5347d931a43d3b5d
Bryan Clark <clark.a.bryan@gmail.com>
2024-12-03 22:06:12 -0700

    AT-12: Fix failing test

--- src/test/groovy/edu/wgu/activitytracker/services/ActivityServiceTest.groovy
+++ src/test/groovy/edu/wgu/activitytracker/services/ActivityServiceTest.groovy
@@ -1,16 +1,19 @@
 package edu.wgu.activitytracker.services
 
+import edu.wgu.activitytracker.datahandlers.ActivityMapper
 import edu.wgu.activitytracker.repositories.ActivityRepository
 import spock.lang.Specification
 
 class ActivityServiceTest extends Specification {
 
+    private UserService mockUserService
     private ActivityRepository mockActivityRepository
+    private ActivityMapper mockActivityMapper
     private ActivityService activityService
 
     def setup() {
         mockActivityRepository = Mock(ActivityRepository.class)
-        activityService = new ActivityService(mockActivityRepository)
+        activityService = new ActivityService(mockUserService, mockActivityRepository, mockActivityMapper)
     }
 
     def "GetAllActivities test"() {
